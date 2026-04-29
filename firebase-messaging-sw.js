e080535d0e85429d77f78383099e484836ce2e54
Bacher86 <uriel.bacher@gmail.com>
2026-04-29 12:34:45 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts('https://www.gstatic.com/firebasejs/9.17.1/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/9.17.1/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "TU_API_KEY",
+  apiKey: "AIzaSyD4pg_8j_CbgeqA1x8IOqn3CxpXepVWQtI",
   projectId: "app-iosefcaro",
   messagingSenderId: "651320124248",
   appId: "1:651320124248:web:97c8ca00e29114e1983b2d"
@@ -11,10 +11,13 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
+  console.log('Mensaje recibido en segundo plano:', payload);
+  
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: 'logo.png.png'
+    icon: 'logo.png' 
   };
+
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
